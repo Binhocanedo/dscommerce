defb3a835ed6a8fdeda9e5b467e729593cb9a9e7
FÁBIO CANEDO LEÃO <236462@facens.br>
2024-04-10 11:19:06 -0300

    initial commit

---
+++ src/main/java/com/fabiocanedo/dscommerce/entities/Order.java
@@ -0,0 +1,64 @@
+package com.fabiocanedo.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant momemt;
+
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order(){}
+
+    public Order(Long id, Instant momemt, OrderStatus status, User client) {
+        this.id = id;
+        this.momemt = momemt;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMomemt() {
+        return momemt;
+    }
+
+    public void setMomemt(Instant momemt) {
+        this.momemt = momemt;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+}
