0d95548f702bce26d5f419ea307ee16a1f198444
FÁBIO CANEDO LEÃO <236462@facens.br>
2024-08-28 06:27:17 -0300

    Correcao na classe UserService

--- src/main/java/com/fabiocanedo/dscommerce/services/UserService.java
+++ src/main/java/com/fabiocanedo/dscommerce/services/UserService.java
@@ -20,7 +20,7 @@ public class UserService implements UserDetailsService {
     private UserRepository repository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
         if(result.isEmpty()){
             throw new UsernameNotFoundException("Email not found");
