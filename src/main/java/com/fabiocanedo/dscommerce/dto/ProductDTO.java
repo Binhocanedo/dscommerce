f58edc9aa36dbbce0ed6ac111fbabe2db1a04c20
FÁBIO CANEDO LEÃO <236462@facens.br>
2024-08-30 07:59:27 -0300

    Fix validation

--- src/main/java/com/fabiocanedo/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/fabiocanedo/dscommerce/dto/ProductDTO.java
@@ -19,8 +19,10 @@ public class ProductDTO {
     @NotBlank(message = "Campo requerido")
     private String description;
 
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
+
     private String imgUrl;
 
     @NotEmpty(message = "Deve ter pelo menos uma categoria")
